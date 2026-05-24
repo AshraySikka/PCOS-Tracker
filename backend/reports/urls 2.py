c9d6b9462660f06da97f1b0a25e2b8af4735cf50
Ashray Sikka <ashray15.sikka@gmail.com>
2026-05-24 15:28:46 -0400

    chore: remove duplicate and misplaced files

--- backend/reports/urls 2.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('weekly/', views.weekly, name='report-weekly'),
-    path('monthly/', views.monthly, name='report-monthly'),
-]
